e2e1247f5755dcbaf92e00659a1b8566ada9d1aa
Cledersonbc <cledersonbc@live.com>
2022-10-26 02:19:37 -0300

    new team test cases

--- cypress/e2e/1-teams/new_team.cy.js
+++ cypress/e2e/1-teams/new_team.cy.js
@@ -1,14 +1,62 @@
 /// <reference types="cypress" />
 
-describe('1 - Validar novos times', () => {
+describe('1 - Validar novo time', () => {
 
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('Cadastro de colaborador', () => {
-        const worker = require('../../fixtures/worker_example.json')
+    it('1.1 - Cadastro de colaborador Front-end', () => {
+        const teamName = 'Front-end'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
 
+        validateWorkerRegister(worker, teamName)
+
+    })
+
+    it('1.2 - Cadastro de colaborador Back-end', () => {
+        const teamName = 'Back-end'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    it('1.3 - Cadastro de colaborador UX & Design', () => {
+        const teamName = 'UX & Design'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    it('1.4 - Cadastro de colaborador Mobile', () => {
+        const teamName = 'Mobile'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    it('1.5 - Cadastro de colaborador DevOps', () => {
+        const teamName = 'DevOps'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    it('1.6 - Cadastro de colaborador Data Science', () => {
+        const teamName = 'Data Science'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    it('1.7 - Cadastro de colaborador Inovação e Gestão', () => {
+        const teamName = 'Inovação e Gestão'
+        const worker = require('../../fixtures/worker_team.json').find((mock) => mock.teamName === teamName)
+
+        validateWorkerRegister(worker, teamName)
+    })
+
+    const validateWorkerRegister = (worker, teamName) => {
         cy.get('.main-form form input[placeholder="Digite seu nome"]').type(worker.name)
         cy.get('.main-form form input[placeholder="Digite seu cargo"]').type(worker.position)
         cy.get('.main-form form input[placeholder="Digite o endereço da imagem"]').type(worker.image)
@@ -16,10 +64,9 @@ describe('1 - Validar novos times', () => {
         cy.get('.main-form form button').contains('Enviar').click()
 
         cy.get('.team')
-            .should('exist', worker.teamName)
+            .should('exist', teamName)
             .find('.worker')
             .should('contain', worker.name)
-
-    })
+    }
 
 })
