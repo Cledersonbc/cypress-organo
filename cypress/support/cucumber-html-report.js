de0d6502eebc2bc9ca4a0b279c4e14d8a25d91cb
Cledersonbc <cledersonbc@live.com>
2023-04-07 00:57:54 -0300

    update: html reporter

---
+++ cypress/support/cucumber-html-report.js
@@ -0,0 +1,18 @@
+const report = require("multiple-cucumber-html-reporter")
+const os = require('os')
+
+report.generate({
+    jsonDir: "cypress/cucumber-json",
+    reportPath: "cypress/reports/cucumber-htmlreport.html",
+    metadata: {
+        browser: {
+            name: "Firefox",
+            version: "110",
+        },
+        device: "Local test machine",
+        platform: {
+            name: os.type(),
+            version: os.release(),
+        },
+    },
+})
